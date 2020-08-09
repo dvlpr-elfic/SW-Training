49c321012eba8410f2a332b6ba65b2258243e681
zziri <jihzang@naver.com>
2020-08-09 18:35:29 +0900

    7568 덩치.py: 들여쓰기 수정

--- ProblemSolving/BOJ/7568 덩치.py
+++ ProblemSolving/BOJ/7568 덩치.py
@@ -1,30 +1,30 @@
 
 def larger(cur, side):
-return (side[0] > cur[0]) and (side[1] > cur[1])
+    return (side[0] > cur[0]) and (side[1] > cur[1])
 
 
 def solve(arr):
-ret = []
-for cur in range(len(arr)):
-count = 1
-for side in range(len(arr)):
-if side == cur:
-continue
-if larger(arr[cur], arr[side]):
-count += 1
-ret.append(count)
-return ret
+    ret = []
+    for cur in range(len(arr)):
+        count = 1
+        for side in range(len(arr)):
+            if side == cur:
+                continue
+            if larger(arr[cur], arr[side]):
+                count += 1
+        ret.append(count)
+    return ret
 
 
 n = int(input())
 db = []
 for _ in range(n):
-w, h = tuple(input().split(' '))
-db.append((w, h))
+    w, h = tuple(input().split(' '))
+    db.append((w, h))
 
 
 answer = solve(db)
 
 for item in answer:
-print("{}".format(item), end=' ')
+    print("{}".format(item), end=' ')
 print()
