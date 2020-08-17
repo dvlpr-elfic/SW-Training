3ecdb86684a0b5a2950900cd98bdefcc49d1397b
zziri <jihzang@naver.com>
2020-08-17 22:56:29 +0900

    1476 날짜 계산.py

---
+++ ProblemSolving/BOJ/1476 날짜 계산.py
@@ -0,0 +1,1 @@
+def get(x, base):  if x % base == 0:    return base  else:    return (x % base)def solve(e, s, m):  ret = 1  while True:    if (e == get(ret, 15)) and (s == get(ret, 28)) and (m == get(ret, 19)):      break    else:      ret += 1  return retE, S, M = tuple(map(int, input().split()))print(solve(E, S, M))
