7acc500cb1731bdf5cf34fa55e0bfc2e9e7bb0de
zziri <jihzang@naver.com>
2020-08-30 11:35:35 +0900

    15650 N과 M(2).py

--- ProblemSolving/BOJ/15650 N과 M(2).py
+++ ProblemSolving/BOJ/15650 N과 M(2).py
@@ -1,2 +1,41 @@
-n, m = tuple(map(int, input().split()))
 
+def list_copy(src):
+    ret = []
+    for item in src:
+        ret.append(item)
+    return ret
+
+
+def perm(depth, n, m, start, temp, answer):
+    if depth == m:
+        answer.append(list_copy(temp))
+        return
+
+    for i in range(start, n+1):
+        if not picked[i]:
+            picked[i] = True
+            temp.append(i)
+            perm(depth+1, n, m, i+1, temp, answer)
+            picked[i] = False
+            temp.pop()
+
+
+def solve(n, m):
+    answer = []
+    perm(0, n, m, 1, [], answer)
+    return answer
+
+
+def main():
+    n, m = map(int, input().split())
+    global picked
+    picked = [False]*(n+1)
+    answer = solve(n, m)
+    for arr in answer:
+        for item in arr:
+            print(item, end=' ')
+        print()
+
+
+if __name__ == "__main__":
+    main()
