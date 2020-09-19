8cae2914f9b9f509c1da7bc13d458e3b446af6b5
zziri <jihzang@naver.com>
2020-09-19 17:37:52 +0900

    11726 2xn 타일링.py 런타임 에러 수정

--- ProblemSolving/BOJ/11726 2xn 타일링.py
+++ ProblemSolving/BOJ/11726 2xn 타일링.py
@@ -3,13 +3,13 @@ def dp(n):
     if n == 1 or n == 2:
         return n
     for cur in range(3, n+1):
-        memo[cur] = (memo[cur-1] + memo[cur-2])%10007
+        memo[cur] = (memo[cur-2] + memo[cur-1])%10007
     return memo[n]
 
 def main():
     n = int(input())
     global memo
-    memo = [0]*(n+1)
+    memo = [0]*(1001)
     memo[1] = 1
     memo[2] = 2
     return dp(n)
